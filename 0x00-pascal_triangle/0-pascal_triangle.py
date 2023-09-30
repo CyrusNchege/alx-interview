60140da3cc97e4adb44d9dfa972003455462fe72
CyrusNchege <cyruschegecc@gmail.com>
2023-09-30 10:48:50 +0300

    Refactor var names

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,16 +2,20 @@
 
 def pascal_triangle(n):
     """
-    Returns a list of lists of integers representing the Pascal’s triangle of n
+    Returns a list of lists of integers representing the Pascal's triangle of n.
     """
-    pascal = []
     if n <= 0:
         return []
-    pascal = [[1]]
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(pascal[i - 1][j - 1] + pascal[i - 1][j])
-        row.append(1)
-        pascal.append(row)
-    return pascal
+
+    triangle = []
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                value = triangle[i - 1][j - 1] + triangle[i - 1][j]
+                row.append(value)
+        triangle.append(row)
+
+    return triangle
